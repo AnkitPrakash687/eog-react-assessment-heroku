88c3fb13b92da8eae517a3eb5ed57061f915e9af
Ankit Prakash <ankit.prakash687@gmail.com>
2020-02-08 13:51:25 -0500

    update server.js

--- codeword.js
+++ codeword.js
@@ -3,6 +3,11 @@ var path = require('path');
 require('dotenv').config();
 var app = express();
 
+app.set('views', path.join(__dirname, 'client/build'));
+app.use(express.static(path.join(__dirname, 'client/build')));
+app.engine("html", require('ejs').renderFile)
+app.set('view engine', 'html');
+
 app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname+'/client/build/index.html'));
 });
