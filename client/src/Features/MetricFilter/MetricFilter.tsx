d73667f3b031f0fd46ee63b296d29b1e63f770ed
Ankit Prakash <ankit.prakash687@gmail.com>
2020-02-12 11:06:27 -0500

    update

--- client/src/Features/MetricFilter/MetricFilter.tsx
+++ client/src/Features/MetricFilter/MetricFilter.tsx
@@ -1,129 +1,114 @@
-import React, { useEffect } from 'react';
-import { useDispatch } from 'react-redux';
-import { filterActions } from './reducer';
-import { Provider, createClient, useQuery } from 'urql';
-import LinearProgress from '@material-ui/core/LinearProgress';
+import React from 'react';
+import { useSelector } from 'react-redux';
+import { Provider, createClient, dedupExchange, fetchExchange, subscriptionExchange } from 'urql';
+import { useSubscription } from 'urql';
+import { SubscriptionClient } from 'subscriptions-transport-ws';
 import { makeStyles } from '@material-ui/core/styles';
+import { IState } from '../../store';
+import MetricCard from '../../components/MetricCard';
+import LineGraph from '../../components/LineGraph';
+import grey from '@material-ui/core/colors/grey';
 import Box from '@material-ui/core/Box';
-import Autocomplete from '@material-ui/lab/Autocomplete';
-import TextField from '@material-ui/core/TextField';
+import Typography from '@material-ui/core/Typography';
+
+const SECONDS = 30 * 60,
+  RATE = 1.3;
+const dataLength = Math.floor(SECONDS / RATE);
+
+const subscriptionClient = new SubscriptionClient('wss://react.eogresources.com/graphql', {
+  reconnect: true,
+});
 
 const client = createClient({
   url: 'https://react.eogresources.com/graphql',
+  exchanges: [
+    dedupExchange,
+    fetchExchange,
+    subscriptionExchange({
+      forwardSubscription: operation => subscriptionClient.request(operation),
+    }),
+  ],
 });
 
 const useStyles = makeStyles(theme => ({
-  searchBox: {
-    minHeight: 30,
-    width: '22rem',
-    display: 'flex',
-    flexDirection: 'row',
+  root: {
+    padding: theme.spacing(2),
+    background: grey[200],
+    minHeight: window.innerHeight,
   },
 }));
 
-const query = `
-query {
-  getMetrics
-}
-`;
-
+const getFilters = (state: IState) => {
+  const { filters } = state.filter;
+  return {
+    filters,
+  };
+};
 
 export default () => {
   return (
     <Provider value={client}>
-      <MetricFilter />
+      <MetricGraph />
     </Provider>
   );
 };
 
-const MetricFilter = () => {
+const MetricGraph = () => {
   const classes = useStyles();
- // const filterInputRef = useRef<HTMLInputElement>(null);
-  const dispatch = useDispatch();
 
+  const subscription = `
+  subscription{
+    newMeasurement{
+      metric
+      at
+      value
+      unit
+    }
+  }
+  `;
 
-  const [result] = useQuery({
-    query,
+  //  const [newMeasurement] = useSubscription({query: subscription})
+  //  //console.log(newMeasurement.data)
+  //  let subData = newMeasurement.data
+  //  if(subData){
+  //   console.log(subData.newMeasurement)
+  //  }
+
+  const [result] = useSubscription({ query: subscription }, (measurements = [], res) => {
+    return [...measurements, res.newMeasurement];
   });
-
-  const { fetching, data, error } = result;
-
-  // useEffect(() => {
-  //     console.log(data.getMetrics)
-  //  dispatch(filterActions.initializeFilterNotSelected({metrics: data.getMetrics}));
-
-  // }, []);
-
-  useEffect(() => {
-    if (error) {
-      dispatch(filterActions.filterApiErrorReceived({ error: error.message }));
-      return;
-    }
-    if (!data) return;
-  }, [dispatch, data, error]);
-
-  // const handleKeyUp = (name: string) => (event: any) => {
-  //   if (event.keyCode === 46) {
-  //     let name = filters[filters.length - 1];
-  //     if (name !== ' ') {
-  //       dispatch(filterActions.removeFilter({ selectedFilter: name }));
-  //     }
-  //   }
-  // };
-
-  // const focusInput = () => {
-  //   if (filterInputRef && filterInputRef.current) {
-  //     filterInputRef.current.value = '';
-  //     filterInputRef.current.focus();
-  //   }
-  // };
-
-  // const handleClickList = (event: any) => {
-  //   let filterInput = event.target.value;
-  //   if (data.getMetrics.includes(event.target.value)) {
-  //     dispatch(filterActions.addFilter({ selectedFilter: event.target.value }));
-  //     focusInput();
-  //   } else if (filterInput === 'No options') {
-  //     focusInput();
-  //   }
-  // };
-
-  const handleChange = (name: string) => (event: any, value: any) => {
-    if (name === 'metricFilter') {
-      console.log(value)
-      let filters = value.filter((metric:string)=>{ return metric !== ''})
-      dispatch(filterActions.setFilter({ filters: filters }));
-    }
-  };
-
-  // const handleDeleteList = (name: string) => (event: any) => {
-  //   dispatch(filterActions.removeFilter({ selectedFilter: name }));
-  //   if (name === 'clearAll') {
-  //     dispatch(filterActions.clearAll());
-  //   }
-  // };
-
-  if (fetching) return <LinearProgress />;
+  let newData = result.data;
+  // console.log(newData)
+  const { filters } = useSelector(getFilters);
 
   return (
-    <div>
-      <Box id="searchBox" alignItems="flex-end" >
-        
-        <Autocomplete
-        className={classes.searchBox}
-        multiple
-        autoHighlight
-        id="metric-filter"
-        options={data.getMetrics}
-        filterSelectedOptions
-        onChange={handleChange('metricFilter')}
-        renderInput={(params:any) => (
-          <TextField {...params} margin="dense" variant="outlined" placeholder="Select Metrics..." fullWidth />
-        )}
-      />
-  
-      </Box>
-    
-    </div>
+    <>
+      {filters.length > 0 && filters[0] !== '' ? (
+        <Box display="flex" m={2} flexDirection="row" flexWrap="wrap" className={classes.root}>
+          {filters.map((filter, index) => {
+            if (filter !== '') {
+              return (
+                <Box key={index} display="flex" m={2} flexDirection="column">
+                  <MetricCard metricName={filter} />
+                  <LineGraph
+                    metricName={filter}
+                    newData={newData
+                      .filter((f: any) => {
+                        return f.metric === filter;
+                      })
+                      .slice(dataLength * -1)}
+                  />
+                </Box>
+              );
+            }
+            return false;
+          })}
+        </Box>
+      ) : (
+        <Box style={{ width: window.innerWidth }} display="flex" justifyContent="center" alignItems="center">
+          <Typography variant="h6">NO FILTER SELECTED</Typography>
+        </Box>
+      )}
+    </>
   );
 };
